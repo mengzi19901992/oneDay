2d49cf18116ec483917786511a9a0c8b36734e00
mengzi <you@example.comzhangwenmeng@feinno.com>
2022-04-24 18:19:27 +0800

    1

--- javascript/suanfa.js
+++ javascript/suanfa.js
@@ -542,3 +542,30 @@ var bubbleSort = function(arr){
 console.log(bubbleSort([5,3,6,1,4,2]))
 
 
+// 例如：
+
+// A -> 1
+// B -> 2
+// C -> 3
+// ...
+// Z -> 26
+// AA -> 27
+// AB -> 28
+// ...
+
+// 示例 1：
+
+// 输入：columnNumber = 1
+// 输出："A"
+// 示例 2：
+
+// 输入：columnNumber = 28
+// 输出："AB"
+// 示例 3：
+
+// 输入：columnNumber = 701
+// 输出："ZY"
+// 示例 4：
+
+// 输入：columnNumber = 2147483647
+// 输出："FXSHRXW"
